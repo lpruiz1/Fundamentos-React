a1723543c6508927a716ee7ee61def97e0f95c54
lpruiz1 <2023004864@ifam.edu.br>
2025-04-25 22:08:36 -0400

    Personalização de Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css"
 import React from "react";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
@@ -6,24 +7,30 @@ import Aleatorio from "./components/basicos/Aleatorio";
 import Card from "./components/layout/Card";
 
 export default (_) => (
-  <div id="app">
+  <div className="App">
     <h1>Fundamentos React</h1>
-    <Card titulo="#04 - Desafio Aleatório">
-      <Aleatorio min={0} max={1000000} />
-    </Card>
-    <Card titulo="#03 - Fragmento">
-      <Fragmento />
-    </Card>
-    <Card titulo="#02 - ComParâmetro">
-      <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
-      <ComParametro
-        titulo="Situação do Aluno"
-        aluno="Maria da Silva"
-        nota={9.9}
-      />
-    </Card>
-    <Card titulo=" #01 - Primeiro Componente">
-      <Primeiro></Primeiro>
-    </Card>
+    <div className="Cards">
+      <Card titulo="#04 - Desafio Aleatório" color="#5cce00">
+        <Aleatorio min={0} max={1000000} />
+      </Card>
+      <Card titulo="#03 - Fragmento" color="#6814ff">
+        <Fragmento />
+      </Card>
+      <Card titulo="#02 - ComParâmetro" color="#ff6814">
+        <ComParametro
+          titulo="Situação do Aluno"
+          aluno="Pedro Silva"
+          nota={9.3}
+        />
+        <ComParametro
+          titulo="Situação do Aluno"
+          aluno="Maria da Silva"
+          nota={9.9}
+        />
+      </Card>
+      <Card titulo=" #01 - Primeiro Componente" color="#005cce">
+        <Primeiro></Primeiro>
+      </Card>
+    </div>
   </div>
 );
